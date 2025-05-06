133aa564452df46e7ed889848e026615e7d5f05a
Vinit <vinitm722@gmail.com>
2025-05-06 16:42:02 +0530

    Day 2: JSON normalization, data transformation, and basic visualization

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import json
 import requests
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
-response = requests.get("https://disease.sh/v3/covid-19/countries/india")
-data = response.json()
-print(f"Total Cases of Covid-19 in India is {data['cases']}")
+url = "https://disease.sh/v3/covid-19/countries"
 
-print(f"Total Death of Covid-19 in India is {data['deaths']}")
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an HTTPError for bad responses
+    data = response.json()
+    
+    #print(f"Total Cases of Covid-19 in India is {data['cases']}")
+    #print(f"Total Death of Covid-19 in India is {data['deaths']}")
+    
 
-print(data['deathsPerOneMillion'])
+    
+    # Save JSON response to a file inside the data folder
+    with open("data/global_covid_data.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+    logging.info("JSON data saved successfully.")
+
+except requests.exceptions.HTTPError as http_err:
+    logging.error(f" HTTP error occurred: {http_err}")
+except Exception as err:
+    logging.error(f" An error occurred: {err}")
